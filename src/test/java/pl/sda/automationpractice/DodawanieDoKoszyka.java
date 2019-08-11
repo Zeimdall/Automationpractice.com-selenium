b9ab72c88d36902d51a8ad7ea675b36c89a9b2f5
Piotr <Piotrzych98@gmail.com>
2019-08-11 15:15:10 +0200

    Klikanie na produkt (przeglądarka otwarta w małym oknie)v2 - selenium Automationpractice.com

--- src/test/java/pl/sda/automationpractice/DodawanieDoKoszyka.java
+++ src/test/java/pl/sda/automationpractice/DodawanieDoKoszyka.java
@@ -1,33 +1,47 @@
 package pl.sda.automationpractice;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
 
 public class DodawanieDoKoszyka {
     ChromeDriver przeglądarka;
 
+    @Before
+    public void otwórzStronę() {
+        System.setProperty("webdriver.chrome.driver", "C:/Chromedriver/chromedriver.exe");
+        przeglądarka = new ChromeDriver();
+        przeglądarka.manage().window().setSize(new Dimension(1024, 768));
+        przeglądarka.get("http://google.com");
+        przeglądarka.get("http://automationpractice.com");
+    }
 
     @Test
     public void dodajProduktDoKoszyka() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "C:/Chromedriver/chromedriver.exe");
-        przeglądarka = new ChromeDriver();
-        //przeglądarka.manage().window().maximize();
-        przeglądarka.get("http://automationpractice.com");
+
 
        List<WebElement> listaProduktow = przeglądarka.findElements(
                (By.cssSelector("#homefeatured .ajax_add_to_cart_button")));
 
+        Actions actions = new Actions(przeglądarka);
+        actions.moveToElement(listaProduktow.get(0)).perform();
 
+        Thread.sleep(1500);
        listaProduktow.get(0).click();
 
-        Thread.sleep(3000);
+    }
+    @After
+    public void zamknijStronę() throws InterruptedException {
+
+            Thread.sleep(3000);
         przeglądarka.quit();
-
-
     }
 
 }
