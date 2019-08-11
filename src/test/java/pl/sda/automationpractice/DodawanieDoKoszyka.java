d7ef052c8f72f05a73f395c710d9fe493cc35b74
Piotr <Piotrzych98@gmail.com>
2019-08-11 15:00:40 +0200

    Klikanie na produkt (przeglądarka otwarta w małym oknie) - selenium Automationpractice.com

--- src/test/java/pl/sda/automationpractice/DodawanieDoKoszyka.java
+++ src/test/java/pl/sda/automationpractice/DodawanieDoKoszyka.java
@@ -1,11 +1,33 @@
 package pl.sda.automationpractice;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class DodawanieDoKoszyka {
+    ChromeDriver przeglądarka;
+
 
     @Test
-    public void dodajProduktDoKoszyka() {
+    public void dodajProduktDoKoszyka() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:/Chromedriver/chromedriver.exe");
+        przeglądarka = new ChromeDriver();
+        //przeglądarka.manage().window().maximize();
+        przeglądarka.get("http://automationpractice.com");
+
+       List<WebElement> listaProduktow = przeglądarka.findElements(
+               (By.cssSelector("#homefeatured .ajax_add_to_cart_button")));
+
+
+       listaProduktow.get(0).click();
+
+        Thread.sleep(3000);
+        przeglądarka.quit();
+
 
     }
+
 }
